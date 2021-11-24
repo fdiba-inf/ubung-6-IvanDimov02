2a320575928700344dbccc3a8dcebba28e10371c
IvanDimov02 <>
2021-11-24 19:23:57 +0000

    test

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -7,17 +7,18 @@ public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        boolean valid = true;
+        boolean valid = false;
+        System.out.println("Enter password: "); 
 
         do{
           String password = input.nextLine();
           if (password.length() < 8) {
-            valid = false;
+            valid = true;
           }
           for (int i = 0; i < password.length(); i++) {
-            if (!(Character.isLetterOrDigit(password.charAt(i)))) {
-              valid = false;
-              break;
+            if (Character.isLetterOrDigit(password.charAt(i))) {
+              valid = true;
+              
             }
           }
           int counter = 0;
@@ -26,8 +27,8 @@ public class PasswordCheck {
               counter ++;
             }
           }
-          if ( counter < 2) {
-            valid = false;
+          if ( counter >= 2) {
+            valid = true;
           }
         } while (valid == false);
         System.out.println("Password valid!");
