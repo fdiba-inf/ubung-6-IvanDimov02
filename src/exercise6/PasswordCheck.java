8e6c86df24114b58ecfad0368d7182b8165fcdd8
IvanDimov02 <>
2021-11-24 19:07:02 +0000

    Some message

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -1,12 +1,14 @@
 package exercise6;
 
 import java.util.Scanner;
-import java.lang.Math;
+
 
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         boolean valid = true;
+
         do{
           String password = input.nextLine();
           if (password.length() < 8) {
